68f7e1837a0c2536cc52ab8f8e0e0ac25546744b
KingBrezzz <bobnbb046@gmail.com>
2026-09-01 22:44:14 +0700

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,4 +1,23 @@
-window.TICKSCOPE_CONFIG = {
-    apiBase: "",
-    token: ""
-};
+(function () {
+
+    const savedApi =
+        localStorage.getItem(
+            "tickscope_api"
+        );
+
+    const savedToken =
+        localStorage.getItem(
+            "tickscope_token"
+        );
+
+    window.TICKSCOPE_CONFIG = {
+
+        apiBase:
+            savedApi || "",
+
+        token:
+            savedToken || ""
+
+    };
+
+})();
