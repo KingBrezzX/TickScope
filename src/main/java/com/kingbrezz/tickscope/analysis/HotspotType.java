ff2db8b358eec4b37d0ab15e69ce4f376174b04b
KingBrezzz <bobnbb046@gmail.com>
2026-09-01 22:15:09 +0700

    Add HotspotType enum for analysis

---
+++ src/main/java/com/kingbrezz/tickscope/analysis/HotspotType.java
@@ -0,0 +1,12 @@
+package com.kingbrezz.tickscope.analysis;
+
+public enum HotspotType {
+
+    REDSTONE,
+    ENTITY,
+    TILE_ENTITY,
+    CHUNK,
+    BLOCK_UPDATE,
+    FLUID,
+    PLAYER_ACTIVITY
+}
