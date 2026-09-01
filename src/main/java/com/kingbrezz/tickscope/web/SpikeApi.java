738ec3106d48afa57c9eac15e51298d8b70dd2f3
KingBrezzz <bobnbb046@gmail.com>
2026-09-01 22:50:21 +0700

    Create SpikeApi.java

---
+++ src/main/java/com/kingbrezz/tickscope/web/SpikeApi.java
@@ -0,0 +1,25 @@
+package com.kingbrezz.tickscope.web;
+
+import com.kingbrezz.tickscope.TickScope;
+import com.kingbrezz.tickscope.monitor.SpikeEvent;
+
+import java.util.List;
+
+public final class SpikeApi {
+
+    private SpikeApi() {
+    }
+
+    public static List<SpikeEvent> get(
+            TickScope plugin
+    ) {
+
+        if (plugin.getSpikeMonitor() == null) {
+            return List.of();
+        }
+
+        return plugin.getSpikeMonitor()
+                .getHistory()
+                .getEvents();
+    }
+}
