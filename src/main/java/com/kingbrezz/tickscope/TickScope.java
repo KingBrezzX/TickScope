d0248ffa73c3746028f93af40e5ec7d9579c15b1
KingBrezzz <bobnbb046@gmail.com>
2026-09-01 22:13:00 +0700

    Update TickScope.java

--- src/main/java/com/kingbrezz/tickscope/TickScope.java
+++ src/main/java/com/kingbrezz/tickscope/TickScope.java
@@ -1,10 +1,16 @@
 package com.kingbrezz.tickscope;
 
+import com.kingbrezz.tickscope.command.TickScopeCommand;
+import com.kingbrezz.tickscope.monitor.PerformanceMonitor;
+import com.kingbrezz.tickscope.monitor.SpikeDetector;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TickScope extends JavaPlugin {
 
     private static TickScope instance;
+
+    private PerformanceMonitor performanceMonitor;
+    private SpikeDetector spikeDetector;
 
     @Override
     public void onEnable() {
@@ -12,9 +18,21 @@ public final class TickScope extends JavaPlugin {
 
         saveDefaultConfig();
 
+        performanceMonitor = new PerformanceMonitor(this);
+        performanceMonitor.start();
+
+        spikeDetector = new SpikeDetector(this, performanceMonitor);
+        spikeDetector.start();
+
+        TickScopeCommand command = new TickScopeCommand(this);
+
+        if (getCommand("tickscope") != null) {
+            getCommand("tickscope").setExecutor(command);
+        }
+
         getLogger().info("=================================");
         getLogger().info(" TickScope 1.0.0");
-        getLogger().info(" Server Performance Monitor");
+        getLogger().info(" Performance Monitor & Analyzer");
         getLogger().info(" Author: KingBrezz");
         getLogger().info("=================================");
         getLogger().info("TickScope enabled successfully.");
@@ -22,11 +40,24 @@ public final class TickScope extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        if (spikeDetector != null) {
+            spikeDetector.stop();
+        }
+
         getLogger().info("TickScope disabled.");
+
         instance = null;
     }
 
     public static TickScope getInstance() {
         return instance;
+    }
+
+    public PerformanceMonitor getPerformanceMonitor() {
+        return performanceMonitor;
+    }
+
+    public SpikeDetector getSpikeDetector() {
+        return spikeDetector;
     }
 }
